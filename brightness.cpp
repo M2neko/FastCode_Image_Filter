1b0d3bd8621b6dbf737b3f5754c5b8b32336a273
M2neko <56237903+M2neko@users.noreply.github.com>
2021-10-29 23:11:43 -0700

    Update brightness.cpp

--- brightness.cpp
+++ brightness.cpp
@@ -10,33 +10,6 @@ using namespace std;
 using namespace cv;
 using namespace chrono;
 
-__m256 b;
-__m256 q;
-
-void recursion(int size, float* x, float* y)
-{
-
-	if (size == 32)
-	{
-		_mm256_store_ps(x, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(x), b), q));
-		_mm256_store_ps(x + 8, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(x + 8), b), q));
-
-		_mm256_store_ps(x + 16, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(x + 16), b), q));
-		_mm256_store_ps(x + 24, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(x + 24), b), q));
-
-		_mm256_store_ps(y, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(y), b), q));
-		_mm256_store_ps(y + 8, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(y + 8), b), q));
-
-		_mm256_store_ps(y + 16, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(y + 16), b), q));
-		_mm256_store_ps(y + 24, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(y + 24), b), q));
-		return;
-	}
-
-	recursion(size / 2, x, y);
-	recursion(size / 2, x + size / 2, y + size / 2);
-
-}
-
 void nothing(int x, void *data) {}
 
 void brightness(Mat img, int times)
@@ -48,11 +21,10 @@ void brightness(Mat img, int times)
 
 		cvtColor(img, hsv, COLOR_BGR2HSV);
 		float val = 38;
-		if (TEST_MODE)
-			cin >> val;
+		if (TEST_MODE) cin >> val;
 		val = val / 100.0;
-		b = _mm256_set1_ps(val);
-		q = _mm256_set1_ps(255.0);
+		__m256 b = _mm256_set1_ps(val);
+		__m256 q = _mm256_set1_ps(255.0);
 
 		Mat channels[3];
 		split(hsv, channels);
@@ -66,22 +38,20 @@ void brightness(Mat img, int times)
 		float *x = &(S.at<float>(0, 0));
 		float *y = &(V.at<float>(0, 0));
 
-		// for (int i = 0; i < H.size().height * H.size().width; i += 32)
-		// {
-		// 	_mm256_store_ps(x, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(x), b), q));
-		// 	_mm256_store_ps(x + 8, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(x + 8), b), q));
+		for (int i = 0; i < H.size().height * H.size().width; i += 32)
+		{
+			_mm256_store_ps(x + i, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(x + i), b), q));
+			_mm256_store_ps(x + i + 8, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(x + i + 8), b), q));
 
-		// 	_mm256_store_ps(x + 16, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(x + 16), b), q));
-		// 	_mm256_store_ps(x + 24, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(x + 24), b), q));
+			_mm256_store_ps(x + i + 16, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(x + i + 16), b), q));
+			_mm256_store_ps(x + i + 24, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(x + i + 24), b), q));
 
-		// 	_mm256_store_ps(y, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(y), b), q));
-		// 	_mm256_store_ps(y + 8, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(y + 8), b), q));
+			_mm256_store_ps(y + i, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(y + i), b), q));
+			_mm256_store_ps(y + i + 8, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(y + i + 8), b), q));
 
-		// 	_mm256_store_ps(y + 16, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(y + 16), b), q));
-		// 	_mm256_store_ps(y + 24, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(y + 24), b), q));
-		// }
-	
-		recursion((int)(H.size().height) * (int)(H.size().width), x, y);
+			_mm256_store_ps(y + i + 16, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(y + i + 16), b), q));
+			_mm256_store_ps(y + i + 24, _mm256_min_ps(_mm256_mul_ps(_mm256_load_ps(y + i + 24), b), q));
+		}
 
 		H.convertTo(H, CV_8U);
 		S.convertTo(S, CV_8U);
@@ -93,10 +63,9 @@ void brightness(Mat img, int times)
 
 		Mat res;
 		cvtColor(hsvNew, res, COLOR_HSV2BGR);
-		if (TEST_MODE)
-		{
-			imshow("original", img);
-			imshow("image", res);
+		if (TEST_MODE) {
+			imshow("original",img);
+			imshow("image",res);
 			waitKey(0);
 		}
 	}
@@ -105,13 +74,13 @@ void brightness(Mat img, int times)
 int main(int argc, char **argv)
 {
 	int times = atoi(argv[1]);
-	Mat img = imread("image4.jpg");
+	Mat img = imread("image2.jpg");
 	auto start = system_clock::now();
 	brightness(img, times);
 	auto end = system_clock::now();
 	auto duration = duration_cast<microseconds>(end - start);
 	cout << "It takes "
 		 << double(duration.count()) * microseconds::period::num / microseconds::period::den * 1000.0 / double(times)
-		 << " milliseconds" << endl;
+	<< " milliseconds" << endl;
 	return 0;
 }
